171d14a522c77e9b2e69421877223d7e5db27907
Suhani Sharma <suhanis2001@gmail.com>
2022-12-16 14:34:03 +0530

    all functional

---
+++ src/components/ScoreCard.tsx
@@ -0,0 +1,13 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+import { useSelector } from "react-redux";
+import { IGlobalState } from "../store/reducers";
+
+const ScoreCard = () => {
+    const score = useSelector((state: IGlobalState) => state.score);
+    return (
+        <Heading as="h2" size="md" mt={5} mb={5}>Current Score: {score}</Heading>
+    );
+}
+
+export default ScoreCard;
